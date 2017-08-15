fabcfc4a635680111895b1f84b2c2f0d9b21b50f
bananabiz <lee21acc973@163.com>
2017-08-15 14:39:22 +1000

    first upload

---
+++ Assets/Scripts/BallBrain.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBrain : MonoBehaviour {
+    Rigidbody rigi;
+	// Use this for initialization
+	void Start () {
+        rigi = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "bounce")
+        { 
+        print("Hello!");
+        rigi.velocity = ((transform.position - collision.contacts[0].point) * 50);
+        }
+    }
+}
